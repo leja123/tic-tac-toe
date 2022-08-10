ccca17a85137c58b36fe14892a7af534242209f0
Leja Podobnik <leja123@users.noreply.github.com>
2022-08-10 11:35:16 +0200

    grid update

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ int main()
 }
 
 void show_grid(char grid[][3], int n){
-    cout<<grid[0][0]<<" "<<grid[1][0]<<" "<<grid[2][0]<<endl;
-    cout<<grid[0][1]<<" "<<grid[1][1]<<" "<<grid[2][1]<<endl;
-    cout<<grid[0][2]<<" "<<grid[1][2]<<" "<<grid[2][2]<<endl;
+    cout<<" "<<grid[0][0]<<" "<<char(179)<<" "<<grid[1][0]<<" "<<char(179)<<" "<<grid[2][0]<<endl;
+    cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<endl;
+    cout<<" "<<grid[0][1]<<" "<<char(179)<<" "<<grid[1][1]<<" "<<char(179)<<" "<<grid[2][1]<<endl;
+    cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<char(196)<<endl;
+    cout<<" "<<grid[0][2]<<" "<<char(179)<<" "<<grid[1][2]<<" "<<char(179)<<" "<<grid[2][2]<<endl;
 }
