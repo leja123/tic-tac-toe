a33d474e24cbaed0b49b8f08e5104eb5def0ca56
Leja Podobnik <leja123@users.noreply.github.com>
2022-08-10 11:25:10 +0200

    grid

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+void show_grid(char grid[][3], int n);
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    char grid[3][3];
+    show_grid(grid, 3);
     return 0;
+}
+
+void show_grid(char grid[][3], int n){
+    cout<<grid[0][0]<<" "<<grid[1][0]<<" "<<grid[2][0]<<endl;
+    cout<<grid[0][1]<<" "<<grid[1][1]<<" "<<grid[2][1]<<endl;
+    cout<<grid[0][2]<<" "<<grid[1][2]<<" "<<grid[2][2]<<endl;
 }
